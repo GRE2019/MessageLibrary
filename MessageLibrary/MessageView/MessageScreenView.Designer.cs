92d22bef37af6281e4e4de178e4882f02c3ff58e
Cesar Lorenzo <clorenzo@grtle.com>
2019-05-20 10:51:58 -0500

    Edit size form.

--- MessageLibrary/MessageView/MessageScreenView.Designer.cs
+++ MessageLibrary/MessageView/MessageScreenView.Designer.cs
@@ -34,8 +34,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 311);
             this.Name = "MessageScreenView";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
             this.ResumeLayout(false);
 
         }
