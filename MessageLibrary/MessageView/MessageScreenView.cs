2f61396974028780ce6da5a8a956aa81354f3e08
Cesar Lorenzo <clorenzo@grtle.com>
2019-05-20 10:45:58 -0500

    Change screen Name.

--- MessageLibrary/MessageView/MessageScreenView.cs
+++ MessageLibrary/MessageView/MessageScreenView.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MessageView
 {
-    public partial class Form1 : Form
+    public partial class MessageScreenView : Form
     {
-        public Form1()
+        public MessageScreenView()
         {
             InitializeComponent();
         }
